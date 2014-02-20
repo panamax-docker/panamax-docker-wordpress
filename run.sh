921c874c822faf09531520a223d4d9adb0a51a85
vagrant <vagrant@precise64.(none)>
2014-02-20 21:12:06 +0000

    Updating wordpress config for fig

--- run.sh
+++ run.sh
@@ -5,20 +5,22 @@ then
         exit 1
 fi
 
-DB_EXISTS=$(mysql -uadmin -p$DB_PASSWORD -h$DB_PORT_3306_TCP_ADDR -P$DB_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE '"$WORDPRESS_DB_NAME"';" | grep "$WORDPRESS_DB_NAME" > /dev/null; echo "$?")
+DB_EXISTS=$(mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "SHOW DATABASES LIKE 'wordpress';" | grep "wordpress" > /dev/null; echo "$?")
+
+echo DB_EXISTS
 
 if [[ DB_EXISTS -eq 1 ]]; 
 then
-        echo "=> Creating database $WORDPRESS_DB_NAME"
+        echo "=> Creating database wordpress"
         RET=1
         while [[ RET -ne 0 ]]; do
                 sleep 5
-                mysql -uadmin -p$DB_PASSWORD -h$DB_PORT_3306_TCP_ADDR -P$DB_PORT_3306_TCP_PORT -e "CREATE DATABASE $WORDPRESS_DB_NAME"
+                mysql -uroot -p$DB_PASSWORD -h$DB_1_PORT_3306_TCP_ADDR -P$DB_1_PORT_3306_TCP_PORT -e "CREATE DATABASE wordpress"
                 RET=$?
         done
         echo "=> Done!"
 else
-        echo "=> Skipped creation of database $WORDPRESS_DB_NAME – it already exists."
+        echo "=> Skipped creation of database wordpress – it already exists."
 fi
 
 touch /.mysql_db_created
